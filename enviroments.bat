d08d6d8f8343d4a8c120a2ffa54d2a6844f8668a
Cristian Lujan <cxristian.lujan05@gmail.com>
2019-06-25 17:06:18 -0400

    create three files and update file Vagrantfile

---
+++ enviroments.bat
@@ -0,0 +1,14 @@
+@echooff
+set OS_AUTH_URL=http://10.28.3.240:5000/v3
+set OS_IDENTITY_API_VERSION=3
+set OS_TENANT_NAME=2099bc1dc3ec4c76a9cdbab47ea675a7
+set OS_PROJECT_NAME=fj_at09
+set OS_USERNAME=cristianlujan
+set OS_PASSWORD=Pass
+set OS_DOMAIN_NAME=fundacion-jala
+set OS_FLAVOR=student.2.4R
+set OS_IMAGE=ubuntu-18.04-amd64-server_20190215
+set OS_KEY_PAIR_NAME=cristianLujan
+set OS_REGION_NAME=bootstack-jalasoft
+set OS_INITIALS=CRLU
+set OS_PRIVATE_KEY_PATH=C:\\Users\\Admin\\Downloads\\cristianlujan (1).pem
